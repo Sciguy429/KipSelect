d6e2a607f684d90f65164f13b050ca4215eacf41
Sciguy429 <Sciguy429@gmail.com>
2018-12-14 08:59:26 -0600

    More OCD

--- include/console.h
+++ include/console.h
@@ -2,7 +2,7 @@
 
 #include <sstream>
 
-#define SSTR( x ) static_cast< std::ostringstream & >(( std::ostringstream() << std::dec << x ) ).str()
+#define SSTR(x) static_cast< std::ostringstream & > (( std::ostringstream() << std::dec << x )).str()
 
 #define LOG_LEVEL_INFO 0
 #define LOG_LEVEL_WARNING 1
