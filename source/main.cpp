7d36dbc3b94a6bd9113c547d6ef52d721a3a0275
Sciguy429 <sciguy429@gmail.com>
2018-12-13 22:10:50 -0600

    Basic test of the KIP and BCT classes

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,8 @@
 #include "kip.h"
 #include "bct.h"
 
+using namespace std;
+
 bool run = true;
 u64 kDown;
 /*
@@ -70,14 +72,12 @@ int main(int argc, char **argv)
 	BCT bct;
 	consoleInit(NULL);
 	initLocalConsole();
-
-	log("info test", LOG_LEVEL_INFO);
-	log("warning test", LOG_LEVEL_WARNING);
-	log("error test", LOG_LEVEL_ERROR);
-
+	log("Scanning For Kips", LOG_LEVEL_INFO);
 	kip.scanForKip();
+	log(SSTR("Found " << kip.getKipCount() << " Kips"), LOG_LEVEL_INFO);
+	log("Scanning For BCT Values", LOG_LEVEL_INFO);
 	bct.readBCT();
-
+	log(SSTR("Found " << bct.getBCTCount() << " BCT Values"), LOG_LEVEL_INFO);
 	while (appletMainLoop) {
 		hidScanInput();
 		u32 kdown = hidKeysDown(CONTROLLER_P1_AUTO);
