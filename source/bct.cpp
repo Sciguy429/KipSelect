4d81b3e87f92917e02eb1b27f16e43aa45e9682f
Sciguy429 <Sciguy429@gmail.com>
2018-12-20 17:50:19 -0600

    Make sure bctLoaction can equal std::string::npos

--- source/bct.cpp
+++ source/bct.cpp
@@ -8,12 +8,12 @@ void BCT::scanBCT() {
 	std::string bctString((std::istreambuf_iterator<char>(bctIfStream)), (std::istreambuf_iterator<char>()));
 	bctIfStream.close();
 	for (unsigned int i = 0; i < (sizeof(bctTargets) / sizeof(*bctTargets)); i++) {
-		size_t debugModeLocation = bctString.find(bctTargets[i].c_str(), 0) + bctTargets[i].length();
+		size_t debugModeLocation = bctString.find(bctTargets[i].c_str(), 0);
 		if (debugModeLocation != std::string::npos) {
 			unsigned int pos = bctItems.size();
 			bctItems.push_back(bctItem());
 			bctItems[pos].name = bctNames[i];
-			if (bctString[debugModeLocation] - 48 == 1) {
+			if (bctString[debugModeLocation + bctTargets[i].length()] - 48 == 1) {
 				bctItems[pos].enabled = true;
 			}
 			else {
@@ -30,7 +30,7 @@ void BCT::setBCTItemEnabled(unsigned int bctId, bool enabled) {
 	bctIfStream.close();
 	size_t location = bctString.find(bctTargets[bctId].c_str(), 0);
 	if (location != std::string::npos) {
-		bctString[location] = 48 + enabled;
+		bctString[location + bctTargets[bctId].length()] = 48 + enabled;
 		std::ofstream bctOfStream("/atmosphere/BCT.ini");
 		if (bctOfStream.is_open()) {
 			bctOfStream << bctString;
