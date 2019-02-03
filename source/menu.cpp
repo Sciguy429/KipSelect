8ac44d4ffdd181b79e6b7312f057cbf15f0b6515
Sciguy429 <Sciguy429@gmail.com>
2019-02-03 15:38:15 -0600

    Bold detail header

--- source/menu.cpp
+++ source/menu.cpp
@@ -210,12 +210,14 @@ void MENU::drawMenu() {
 		}
 		//~~
 		//DRAW DETAILS WINDOW
-		gfxDrawText(frameBufferTexture, (*mnu)[menuSelected].name.c_str(), mainFont, 905, 178, 18, RGBA8(255, 255, 255, 0));
+		std::ostringstream name;
+		name << '^' << (*mnu)[menuSelected].name.c_str();
+		gfxDrawText(frameBufferTexture, name.str().c_str(), mainFont, 905, 178, 18, RGBA8(255, 255, 255, 0));
 		for (unsigned int i = 0; i < (*mnu)[menuSelected].details.size(); i++) {
 			unsigned int curY = i * 18 + 202;
-			std::ostringstream ss;
-			ss << '^' << (*mnu)[menuSelected].details[i].prefix << '^' << (*mnu)[menuSelected].details[i].data << (*mnu)[menuSelected].details[i].suffix;
-			gfxDrawTextWrap(frameBufferTexture, ss.str().c_str(), mainFont, 905, curY, 12, RGBA8(255, 255, 255, 0), 370);
+			std::ostringstream detail;
+			detail << '^' << (*mnu)[menuSelected].details[i].prefix << '^' << (*mnu)[menuSelected].details[i].data << (*mnu)[menuSelected].details[i].suffix;
+			gfxDrawTextWrap(frameBufferTexture, detail.str().c_str(), mainFont, 905, curY, 12, RGBA8(255, 255, 255, 0), 370);
 		}
 		if ((*mnu)[menuSelected].status) {
 			gfxBlit(frameBufferTexture, detailEnabled, 972, 652);
