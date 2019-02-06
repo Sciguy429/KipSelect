5fd825016a5a0cc2efb81e5c3de0f29bd8ff7dbb
Sciguy429 <Sciguy429@gmail.com>
2019-02-06 17:21:38 -0600

    Rename error

--- source/ini.cpp
+++ source/ini.cpp
@@ -59,12 +59,12 @@ void INI::loadINI(const char *path) {
 					setValue(key.c_str(), value.c_str());
 				}
 				else {
-					errorThrow(SETTING_INI_PARSE_FAIL, line.c_str());
+					errorThrow(INI_PARSE_FAIL, line.c_str());
 					return;
 				}
 			}
 			else {
-				errorThrow(SETTING_INI_PARSE_FAIL, line.c_str());
+				errorThrow(INI_PARSE_FAIL, line.c_str());
 				return;
 			}
 		}
