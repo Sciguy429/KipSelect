1e4bf71bef734f57eb61a7e9f7138f81d8c55967
Sciguy429 <Sciguy429@gmail.com>
2019-03-02 10:29:45 -0600

    Remove unused includes

--- source/screen/thread/render.cpp
+++ source/screen/thread/render.cpp
@@ -3,9 +3,6 @@
 #include "screen/thread/render.h"
 #include "screen/gfx.h"
 
-#include <vector>
-#include <sstream>
-
 void renderThread(void *vector) {
 	renderThreadControlVector *rTCV = (renderThreadControlVector*)vector;
 	printf("RENDER -- Render Thread Starting\nRENDER -- Running On Core: %d\n", svcGetCurrentProcessorNumber());
