2d508b170373b71b4fa6e514c78dfb75dfcf07ca
Sciguy429 <Sciguy429@gmail.com>
2019-03-03 10:19:56 -0600

    Timer system for render events

---
+++ include/utils/timer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <switch.h>
+#include <vector>
+
+#define NS_TO_TICKS(ns) {armNsToTicks(ns)}
+#define TICKS_TO_NS(ticks) {armTicksToNs(ticks)}
+
+typedef struct {
+	u64 interval;
+	u64 maxCount;
+	u64 lastTick;
+	u64 count;
+	void reset() {
+		lastTick = armGetSystemTick();
+		count = 0;
+	}
+} trigger;
+
+class TIMER {
+public:
+	trigger *createTrigger(u64 interval, u64 maxCount);
+	void clearTriggers();
+	void update();
+private:
+	std::vector<trigger> triggers;
+};
--- source/screen/thread/render.cpp
+++ source/screen/thread/render.cpp
@@ -2,12 +2,20 @@
 
 #include "screen/thread/render.h"
 #include "screen/gfx.h"
+#include "utils/timer.h"
 
 void renderThread(void *vector) {
 	renderThreadControlVector *rTCV = (renderThreadControlVector*)vector;
 	printf("RENDER -- Render Thread Starting\nRENDER -- Running On Core: %d\n", svcGetCurrentProcessorNumber());
+	TIMER timer = TIMER();
+	trigger *test = timer.createTrigger(NS_TO_TICKS(1000000000), 60);
 	while (rTCV->runThread) {
 		//Render
+		timer.update();
+		if (test->count > 0) {
+			test->reset();
+			printf("RENDER -- Trigger Detected!\n");
+		}
 	}
 	printf("RENDER -- Thread Terminating\n");
 }
