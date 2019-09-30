5d4574cd8e7daee17a77cfcc4b80b8b622d40d98
Sciguy429 <Sciguy429@gmail.com>
2019-09-30 14:01:16 -0500

    Remove junk

--- source/screen/screen.cpp
+++
@@ -1,7 +0,0 @@
-#include "screen/screen.h"
-
-SCREEN::SCREEN(unsigned int sizeX, unsigned int sizeY, u32 renderThreadCore) {
-	this->sizeX = sizeX;
-	this->sizeY = sizeY;
-	this->renderThreadCore = renderThreadCore;
-}
